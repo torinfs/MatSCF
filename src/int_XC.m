603cb004311562f43052e071505d4a5c1c46291f
torinfs <torins@uw.edu>
2017-02-14 15:09:13 -0800

    updated int_XC, still no testing

--- src/int_XC.m
+++ src/int_XC.m
@@ -1,46 +1,62 @@
-function [ Vxc, Exc, rhoInt ] = int_XC( basis, P, DGrid )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ Vxc, Exc, rhoInt ] = int_XC( basis, P, DGrid, flag )
+% INT_XC() Calulates the exchange-correlation functional for DFT using the
+% LDA approximation.
+%   INPUTS:
+%       basis = basis set from structure array of mocalc output
+%       P = MxM Density matrix
+%       DGrid = molcular grid
+%       flag = string specifying functional i.e 'SVWN3'
+%
+%   OUTPUTS:
+%       Vxc = MxM exchange-correlation potential matrix
+%       Exc = exchange-correlation energy
+%       rhoInt = Integral of density over allspace. Equal to N electrons.
 
-%Slater Exchange functional
 rhoInt = 0;
-Vxc = zeros(size(out.basis));
+Vxc = zeros(size(basis));
 Exc = 0;
 
+% Calculate rho(r) from grid
 for iGrid = 1:numel(DGrid)
     r = DGrid(iGrid).xyz;
     w = DGrid(iGrid).weights;
     rho = 0;
 
-    base = zeros(numel(out.basis));
+    base = zeros(numel(basis));
     
-    for mu = 1:numel(out.basis)
+    for mu = 1:numel(basis)
         
         musum = 0;
-        xminA = r(1) - out.basis(mu).A(1);
-        yminA = r(2) - out.basis(mu).A(2);
-        zminA = r(3) - out.basis(mu).A(3);
+        xminA = r(1) - basis(mu).A(1);
+        yminA = r(2) - basis(mu).A(2);
+        zminA = r(3) - basis(mu).A(3);
 
-        for k = 1:numel(out.basis(mu).d)
-            musum = musum + out.basis(mu).d(k)*out.basis(mu).N(k)*...
-                exp(-out.basis(mu).alpha(k)*sqrt(xminA^2 + yminA^2 +zminA^2));
+        for k = 1:numel(basis(mu).d)
+            musum = musum + basis(mu).d(k)*basis(mu).N(k)*...
+                exp(-basis(mu).alpha(k)*sqrt(xminA^2 + yminA^2 +zminA^2));
         end
 
-        base(mu) = xminA^(out.basis(mu).a(1)).*yminA^(out.basis(mu).a(2)).*...
-            zminA^(out.basis(mu).a(3)).*musum;
+        base(mu) = xminA^(basis(mu).a(1)).*yminA^(basis(mu).a(2)).*...
+            zminA^(basis(mu).a(3)).*musum;
     end
     
-    for mu = 1:numel(out.basis)
-        for nu = 1:numel(out.basis)
-            rho = rho + out.P(mu,nu)*base(mu)*base(nu);
+    for mu = 1:numel(basis)
+        for nu = 1:numel(basis)
+            rho = rho + P(mu,nu)*base(mu)*base(nu);
         end
     end
     
+    % Final rhoInt value
     rhoInt = rhoInt + w*rho;
-    
-    
-
 end
+
+% get correlation values
+[Ecvwn, Vcvwn] = VWN(rho, flag);
+
+% XC energy value
+Exc = xeslater(rho) + Ecvwn;
+
+
 
 
 end
@@ -54,28 +70,48 @@ function vxs = xvslater(rho)
     vxs = -(3/pi*rho)^1/3;
 end
 
-function [ecvwn, vcvwn] = vwn(rho, flag)
+% Correlation functionals
+function [Ecvwn, Vcvwn] = VWN(rho, flag)
     if strcmp(flag, 'SVWN3')
         A = 0.310907;
         b = 13.0720;
         c = 42.7198;
-        x_0 = -0.409286;
+        x0 = -0.409286;
     elseif strcmp(flag, 'SVWN5')
         A = 0.310907;
         b = 3.72744;
         c = 12.9352;
-        x_0 = -0.10498;
+        x0 = -0.10498;
     else
-        'Unsupported functional specified'
+        disp('Unsupported functional specified')
+        break
     end
     Q = sqrt(4*c-b^2);
-    xi_0 = x_0^2 + b*x_0 + c;
-
+    xi_0 = x0^2 + b*x0 + c;
     x = sqrt((3/(4*pi*rho))^(1/3));
     xi = x^2 + b*x + c;
-    
     eta = arctan(Q/(2*x+b));
     
+    Ecvwn = A*( log((x^2)/xi) + 2*b*eta/Q - (b*x0/xi_0) * ...
+                    ( log(((x-x0)^2)/xi) + (2*(2*x0 + b)*eta)/Q));
+                
+    Vcvwm = -(1/3) * ( (A*(c*(x-x0) - b*x*x0)) / (xi*(x-x0)) ) + Ecvwn;
+    
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
